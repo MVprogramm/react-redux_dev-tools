d0fd7e2b351e22d8e89708d1f74b6b6ef7197ac0
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-07-26 07:58:26 +0300

    add code

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { store, increment, decrement } from "./counter.js";
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+
+
